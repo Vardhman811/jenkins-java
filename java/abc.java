0d00f0bfcb6d1e96a6fdca08f2d0082cb132face
Vardhman811 <52546204+Vardhman811@users.noreply.github.com>
2024-06-17 14:54:58 +0530

    commit
    commit

---
+++ java/abc.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class abc{
+    public static void main(String[] args) {
+        System.out.println("this is java prog");
+        System.out.println("output from java prog");
+        System.out.println(new Date());
+    }
+}
