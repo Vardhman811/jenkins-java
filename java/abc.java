8e0d7774083b3abdcdb6b5ffc30efda53ccde069
Vardhman811 <52546204+Vardhman811@users.noreply.github.com>
2024-06-17 15:11:04 +0530

    Delete java/abc.java

--- java/abc.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class abc{
-    public static void main(String[] args) {
-        System.out.println("this is java prog");
-        System.out.println("output from java prog");
-        System.out.println(new Date());
-    }
-}
